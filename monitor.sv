fffdf538168bee5bf3ba151204ef3c871ee524cf
Siddhant Chhikara <153754448+siddhant-chhikara@users.noreply.github.com>
2026-03-28 18:50:26 +0530

    Add covergroup for FIFO monitoring
    Added a covergroup to monitor write and read enable signals, as well as FIFO status.

--- monitor.sv
+++ monitor.sv
@@ -4,16 +4,27 @@ class monitor;
   scoreboard sb;
   
   bit rd_en_d1 = 0;
+  
+  covergroup cg;
+      coverpoint vif.wr_en;
+      coverpoint vif.rd_en;
+      coverpoint vif.empty;
+      coverpoint vif.full;
+      cross vif.rd_en, vif.wr_en;
+   endgroup
 
   function new(virtual fifo_if vif, scoreboard sb);
     this.vif = vif;
     this.sb  = sb;
+    cg = new();
   endfunction
+  
 
   task run();
 
     forever begin
       @(vif.cb);
+
 
       // WRITE
       if (vif.wr_en && !vif.full) begin
